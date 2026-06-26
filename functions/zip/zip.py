3507c613c5cc5039228c5cb4664077411c7d9561
Andhar7 <boxingprofi@web.de>
2026-06-26 23:19:21 +0200

    added new folder and function the zip

---
+++ functions/zip/zip.py
@@ -0,0 +1,13 @@
+
+
+names  = ["Arjuna", "Devaki", "Rohan"]
+scores = [88,       95,       72     ]
+
+def zip_solver():
+
+    for name, score in zip(names, scores):
+        print(f"Name: {name}, Age: {score}")
+
+zip_solver()
+
+## The Inner World
