bc520f5b6ec4c5153b2124ee063cd7778b61afd5
Andhar7 <boxingprofi@web.de>
2026-06-27 23:12:18 +0200

    added new folder and function the zip

--- functions/zip/zip.py
+++ functions/zip/zip.py
@@ -10,4 +10,37 @@ def zip_solver():
 
 zip_solver()
 
+
+
 ## The Inner World
+array_one = ["Swift", "Django", "TypeScript", "React"]
+array_two = [1, 2, 3, 4, 5, 6, 7]
+def zip_inner_world(*iterables):
+    iterators = [iter(it) for it in iterables]
+
+    while True:
+        result = []
+        for iterator in iterators:
+            try:
+                result.append(next(iterator))
+            except StopIteration:
+                return
+        yield tuple(result)
+
+
+for pair in zip_inner_world(array_two, array_one):
+    print(pair)
+
+# Show me *args
+def show_me_args(*args):
+    if not args:
+        raise ValueError("Life is empty")
+    print(args)
+    print(len(args))
+    print(args[0])
+    print(args[1])
+    print(type(args))
+
+show_me_args(array_one, array_two, names, scores)
+
+
